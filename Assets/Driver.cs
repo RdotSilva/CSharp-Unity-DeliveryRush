7ac4ae4a219872a259c9546071c14d01ed252028
RdotSilva <RyandotSilva@gmail.com>
2021-10-08 13:56:01 -0400

    Tweak steer/move speed to match time delta

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 300;
+    [SerializeField] float moveSpeed = 20;
 
     // Start is called before the first frame update
     void Start()
