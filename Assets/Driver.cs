3df8d7c1958d20ca17009d72f0cff83e9b9d2ce3
RdotSilva <RyandotSilva@gmail.com>
2021-10-08 13:47:31 -0400

    Add logic for foward/reverse car control

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -17,9 +17,10 @@ public class Driver : MonoBehaviour
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
         // Allow a user to steer the car
         transform.Rotate(0, 0, -steerAmount);
         // Drive in the direction the car is facing
-        transform.Translate(0, moveSpeed, 0);
+        transform.Translate(0, moveAmount, 0);
     }
 }
