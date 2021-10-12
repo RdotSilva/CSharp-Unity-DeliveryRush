e0d334106557e1fa26c3a157b9af460f07326a1e
RdotSilva <RyandotSilva@gmail.com>
2021-10-12 13:35:06 -0400

    Update logic for single package pickup

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+    [SerializeField] float destroyDelay = 0.5f;
     private void OnCollisionEnter2D(Collision2D other) 
     {
         // TODO: Add logic to remove user life on collision
@@ -16,10 +17,13 @@ public class Delivery : MonoBehaviour
         // TODO: Add logic to add user life when power up is collected
         
         // Logic for when a user collects a package
-        if (other.tag == "Package") 
+        if (other.tag == "Package" && !hasPackage) 
         {
             Debug.Log("Package collected!");
             hasPackage = true;
+            
+            // Destroy package on pickup
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if (other.tag == "Customer" && hasPackage) 
