eaab5a431811825e920a1c54d78bc7d28e491960
RdotSilva <RyandotSilva@gmail.com>
2021-10-15 15:51:28 -0400

    Set up initial score

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public Text scoreText;
+    private int scoreNum;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        scoreNum = 0;
+        scoreText.text = "Score: " + scoreNum;
     }
 }
